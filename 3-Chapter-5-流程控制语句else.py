3409be91a7b92af67a899f819c365e2f4eb654a1
YangziCao <143952272+YangziCao@users.noreply.github.com>
2023-09-15 23:21:22 +0800

    Add files via upload

--- 3-Chapter-5-流程控制语句else.py
+++ 3-Chapter-5-流程控制语句else.py
@@ -5,9 +5,47 @@ else语句
      ...
      else
      ...
-
+                            1.  if条件表达式不成立时执行else
 2. while...：
     ...
     else：
     ...
+
+3. for...:
+    ...
+    else:
+    ...
+                 2 and 3： 没有碰到break时执行else
+
 '''
+
+#输入密码
+'''
+for item in range(3):
+    passward=input('输入密码：')
+    if passward=='8888':
+        print('密码正确')
+        break
+    else:
+        print('密码错误')
+else:
+    print('对不起，三次密码均输入错误')
+   '''
+a=0
+while a<3:
+    passward=input('输入密码')
+    if passward=='8888':
+        print('密码正确')
+        break
+    else:
+        print('密码错误')
+    a+=1   #改变变量，不可少
+else:
+    print('三次均错误')
+
+'''while循环的执行流程
+四步循环法
+1.初始变化量
+2.条件判断
+3.条件执行体（循环体）
+4.改变变量'''
